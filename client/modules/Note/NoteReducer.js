14ae4fd92f9cabe963b15b1564e74740ac637e2e
Justyna Bejgrowicz <justyna.bejgrowicz@interia.pl>
2018-05-29 20:35:22 +0300

    Add files.

---
+++ client/modules/Note/NoteReducer.js
@@ -0,0 +1,11 @@
+// Initial State
+const initialState = {};
+
+const NoteReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default NoteReducer;
